bb0435dbd6b0199f7ddd825fe8dc15ffc94cb797
shvec <a-s-w@yandex.ru>
2022-02-09 19:41:03 +0300

    Первая версия

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+# Вариант 1 - самый простой чат бот, просто отзывается
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import telebot  # pyTelegramBotAPI	4.3.1
+
+bot = telebot.TeleBot('5187062324:AAHbopf8CawQfNbKYh9DT0SYW-befNjMSNk')  # Создаем экземпляр бота @Ivanov_Ivan_1MD19_bot
+
+# -----------------------------------------------------------------------
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(message, res=False):
+    chat_id = message.chat.id
+
+    bot.send_message(chat_id,
+                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                         message.from_user))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# -----------------------------------------------------------------------
+# Получение сообщений от юзера
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    chat_id = message.chat.id
+    ms_text = message.text
+    bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# -----------------------------------------------------------------------
+bot.polling(none_stop=True, interval=0) # Запускаем бота
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print()
